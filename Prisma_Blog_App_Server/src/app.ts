3e280bb1e32a97a627038567e6fa3e52ca6887cd
tushar <iktushar2020@gmail.com>
2026-01-07 23:13:08 +0600

    Fixing Authentication Errors & Implementing Email Verification

--- Prisma_Blog_App_Server/src/app.ts
+++ Prisma_Blog_App_Server/src/app.ts
@@ -2,8 +2,14 @@ import express, { Application } from 'express';
 import { postRouter } from './modules/post/post.router';
 import { toNodeHandler } from "better-auth/node";
 import { auth } from './lib/auth';
-
+import cors from 'cors';
 const app:Application = express();
+
+app.use(cors({
+    origin: process.env.TRUSTED_ORIGINS?.split(","),
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}))
 
 app.use("/api/auth", toNodeHandler(auth));
 
--- Prisma_Blog_App_Server/src/lib/auth.ts
+++ Prisma_Blog_App_Server/src/lib/auth.ts
@@ -1,13 +1,49 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
-// If your Prisma file is located elsewhere, you can change the path
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // Use true for port 465, false for port 587
+  auth: {
+    user: "",
+    pass: "",
+  },
+});
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
+  trustedOrigins: [process.env.TRUSTED_ORIGINS!],
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        defaultValue: "user",
+        required: false,
+      },
+      phone: {
+        type: "string",
+        required: false,
+      },
+      status: {
+        type: "string",
+        defaultValue: "active",
+        required: false,
+      },
+    },
+  },
   emailAndPassword: {
     enabled: true,
+    autoSignIn: false,
+    requireEmailVerification: true,
+  },
+  emailVerification: {
+    sendVerificationEmail: async ({ user, url, token }, request) => {
+      console.log("********* verification email sent *********");
+    },
   },
 });
