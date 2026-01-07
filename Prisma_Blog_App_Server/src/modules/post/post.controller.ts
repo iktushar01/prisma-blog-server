0b5397f1c0b3ea424e0793d707cd2a31151036ae
tushar <iktushar2020@gmail.com>
2026-01-07 16:03:40 +0600

    modules\post updated

---
+++ Prisma_Blog_App_Server/src/modules/post/post.controller.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+import { postService } from './post.service';
+const createPostHandler = async (req : Request, res : Response) => {
+    try {
+        const result = await postService.createPost(req.body);
+        res.status(201).json(result);
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+}
+
+export const postController = {
+    createPostHandler
+}; 
