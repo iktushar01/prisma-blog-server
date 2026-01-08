0daf2134aa93ab1539833cd5a9ffd25ce9f90ad0
tushar <iktushar2020@gmail.com>
2026-01-08 20:40:19 +0600

    Social Authentication: Sign Up & Sign In with Google

--- Prisma_Blog_App_Server/src/lib/auth.ts
+++ Prisma_Blog_App_Server/src/lib/auth.ts
@@ -271,6 +271,12 @@ export const auth = betterAuth({
       });
     },
   },
+  socialProviders: {
+        google: { 
+            clientId: process.env.GOOGLE_CLIENT_ID as string, 
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
+        }, 
+    },
 });
 
 // Optional: Helper function to test email configuration
