9dcb2daa522e7a117a0b760f7fdc009a7d180f3a
tushar <iktushar2020@gmail.com>
2026-01-15 22:54:23 +0600

    24-9 Authentication Middleware – Part 2 (Role-Based Access Control)

--- Prisma_Blog_App_Server/src/modules/post/post.controller.ts
+++ Prisma_Blog_App_Server/src/modules/post/post.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { postService } from './post.service';
 const createPostHandler = async (req : Request, res : Response) => {
     try {
+        console.log(req.user)
         const result = await postService.createPost(req.body);
         res.status(201).json(result);
     } catch (error) {
--- Prisma_Blog_App_Server/src/modules/post/post.router.ts
+++ Prisma_Blog_App_Server/src/modules/post/post.router.ts
@@ -1,24 +1,58 @@
 import express, { Router, Request, Response, NextFunction } from "express";
 import { postController } from "./post.controller";
-import {auth as authMiddleware} from '../../lib/auth';
+import { auth as authMiddleware } from "../../lib/auth";
+import { User } from "better-auth/types";
 const router: Router = express.Router();
 
-// auth middleware (accepts one or more roles)
-const auth = (...roles: string[]) => {
+
+export enum UserRole {
+  ADMIN = "ADMIN",
+  USER = "USER",
+}
+declare global {
+
+  namespace Express {
+    interface Request {
+      user?: {
+        id: string;
+        email: string;
+        name: string;
+        role: string;
+        emailVerified: boolean;
+      };
+    }
+  }
+}
+const auth = (...roles: UserRole[]) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     //get user session
-    const session = await authMiddleware.api.getSession({ 
-      headers: req.headers as any
+    const session = await authMiddleware.api.getSession({
+      headers: req.headers as any,
     });
+    if (!session || !session.user) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
 
-    console.log(session)
+    if (!session.user.emailVerified) {
+      return res.status(403).json({ message: "Email not verified" });
+    }
+
+    req.user = {
+      id: session.user.id,
+      email: session.user.email,
+      name: session.user.name,
+       role: session.user.role?.toUpperCase() as string,
+      emailVerified: session.user.emailVerified,
+    };
+    if (roles.length && !roles.includes(req.user.role as UserRole)) {
+      return res.status(403).json({ message: "Forbidden" });
+    }
+    
     next();
   };
 };
 
 // use auth middleware on this route
-router.post("/",
-  auth("ADMIN", "USER"),
-  postController.createPostHandler);
+router.post("/", auth(UserRole.USER), postController.createPostHandler);
 
 export const postRouter: Router = router;
