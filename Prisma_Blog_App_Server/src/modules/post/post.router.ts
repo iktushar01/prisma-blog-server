285c89e46219c91e7b4766a88975d5a30a5d771c
tushar <iktushar2020@gmail.com>
2026-01-15 22:01:11 +0600

    24-8 Authentication Middleware – Part 1 (Basic Protection)

--- Prisma_Blog_App_Server/src/modules/post/post.router.ts
+++ Prisma_Blog_App_Server/src/modules/post/post.router.ts
@@ -1,17 +1,24 @@
 import express, { Router, Request, Response, NextFunction } from "express";
 import { postController } from "./post.controller";
-
+import {auth as authMiddleware} from '../../lib/auth';
 const router: Router = express.Router();
 
-// auth middleware
-const auth = () => {
+// auth middleware (accepts one or more roles)
+const auth = (...roles: string[]) => {
   return async (req: Request, res: Response, next: NextFunction) => {
-    console.log("middleware");
+    //get user session
+    const session = await authMiddleware.api.getSession({ 
+      headers: req.headers as any
+    });
+
+    console.log(session)
     next();
   };
 };
 
 // use auth middleware on this route
-router.post("/", auth(), postController.createPostHandler);
+router.post("/",
+  auth("ADMIN", "USER"),
+  postController.createPostHandler);
 
 export const postRouter: Router = router;
