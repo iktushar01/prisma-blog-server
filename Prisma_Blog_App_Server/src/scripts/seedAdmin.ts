b1ff461889ee0e6c6c18bd73bc8e81cac5cc5b3e
tushar <iktushar2020@gmail.com>
2026-01-17 10:02:05 +0600

    Admin Seeding Part 2

--- Prisma_Blog_App_Server/src/scripts/seedAdmin.ts
+++ Prisma_Blog_App_Server/src/scripts/seedAdmin.ts
@@ -4,33 +4,64 @@ import { UserRole } from "../middlewares/auth";
 
 async function seedAdmin() {
   try {
+    const adminEmail = "admin10@gmail.com";
+
     const adminData = {
       name: "admin",
-      email: "admin@gmail.com",
+      email: adminEmail,
       role: UserRole.ADMIN,
       password: "admin123",
+      emailVerified: true,
     };
 
-    //check user exist or not
     const existingUser = await prisma.user.findUnique({
       where: {
-        email: "iktushar2020@gmail.com",
+        email: adminEmail,
       },
     });
+
     if (existingUser) {
       console.log("Admin user already exists");
       return;
     }
- 
-    const signUpAdmin = await fetch("http://localhost:5000/api/auth/sign-up/email",{
-      method : "POST",
-      headers : {
-        "content-type" : "application/json"
-      },
-      body : JSON.stringify(adminData)
-    })
 
+    const signUpAdmin = await fetch(
+      "http://localhost:5000/api/auth/sign-up/email",
+      {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+          origin: "http://localhost:5000", // 🔥 REQUIRED
+        },
+        credentials: "include",
+        body: JSON.stringify(adminData),
+      },
+    );
+
+    if(signUpAdmin.ok){
+      await prisma.user.update(
+        {
+          where : {
+            email: adminData.email
+          },
+          data: {
+            emailVerified : true
+          }
+        }
+      )
+    }
+
+    const data = await signUpAdmin.json();
+    console.log(data);
+
+
+
+    // create admin
+    // email verified false
+    //
   } catch (error) {
     console.error(error);
   }
 }
+
+seedAdmin();
